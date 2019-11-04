579cac57b6486d0d48e922547707aba3a052c812
Camryn Perry <camrynpaigeperry@gmail.com>
2019-11-04 17:06:49 -0500

    styling issues

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,34 +1,34 @@
-import axios from 'axios'
-import history from '../history'
+import axios from 'axios';
+import history from '../history';
 
 /**
  * ACTION TYPES
  */
-const ADD_ITEM = 'ADD_ITEM'
-const REMOVE_ITEM = 'REMOVE_ITEM'
+const ADD_ITEM = 'ADD_ITEM';
+const REMOVE_ITEM = 'REMOVE_ITEM';
 
 /**
  * INITIAL STATE
  */
-const defaultCart = {items: []}
+const defaultCart = {items: []};
 
 /**
  * ACTION CREATORS
  */
-const addedItem = item => ({type: ADD_ITEM, item})
-const removeItem = itemId => ({type: REMOVE_ITEM, itemId})
+const addedItem = item => ({type: ADD_ITEM, item});
+const removeItem = itemId => ({type: REMOVE_ITEM, itemId});
 
 /**
  * THUNK CREATORS
  */
 export const addItem = itemId => async dispatch => {
   try {
-    const res = await axios.get(`/api/items/${itemId}`)
-    dispatch(addedItem(res.data.item))
+    const res = await axios.get(`/api/items/${itemId}`);
+    dispatch(addedItem(res.data.item));
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
-}
+};
 
 /**
  * REDUCER
@@ -36,13 +36,13 @@ export const addItem = itemId => async dispatch => {
 export default function(cart = defaultCart, action) {
   switch (action.type) {
     case ADD_ITEM:
-      return {...cart, items: [...cart.items, action.item]}
+      return {...cart, items: [...cart.items, action.item]};
     case REMOVE_ITEM:
       return {
         ...cart,
         items: cart.items.filter(item => item.id !== action.itemId)
-      }
+      };
     default:
-      return cart
+      return cart;
   }
 }
