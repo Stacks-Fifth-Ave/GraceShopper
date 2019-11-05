1f74677b71d072dfa4c0c4c487a430e31317db3d
Andy Sokolowski <44684409+aesokolowski@users.noreply.github.com>
2019-11-05 15:00:54 -0500

    attempted to get the products.js and users.js files in the api to mirror each other in style, added 404 status codes

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,9 @@
 const router = require('express').Router();
 const {User} = require('../db/models');
 
+const NO_USERS = 'No users found.';
+const USER_NOT_FOUND = 'User not found. Id #: ';
+
 router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
@@ -8,10 +11,27 @@ router.get('/', async (req, res, next) => {
     });
 
     if (users.length < 1) {
-      res.send('no users at the moment');
+      return res.status(404).send(NO_USERS);
     }
 
     res.json(users);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    let userId = req.params.userId;
+    const user = await User.findByPk(userId, {
+      attributes: ['id', 'email']
+    });
+
+    if (user === null) {
+      return res.status(404).send(USER_NOT_FOUND + userId);
+    }
+
+    res.json(user);
   } catch (err) {
     next(err);
   }
