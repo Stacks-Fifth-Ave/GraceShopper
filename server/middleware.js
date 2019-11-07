13a6dfa3eeaf9dd6318865b5de6118e91e92af56
Camryn Perry <camrynpaigeperry@gmail.com>
2019-11-07 17:19:41 -0500

    feat: admin middleware, other middleware in the works

---
+++ server/middleware.js
@@ -0,0 +1,28 @@
+//custom middleware
+
+const isAdminMiddleware = (req, res, next) => {
+  const currentUser = req.user;
+  if (currentUser && currentUser.isAdmin) {
+    next();
+  } else {
+    const error = new Error('Unauthorized request');
+    error.status = 401;
+    next(error);
+  }
+};
+// const isCurrentUserMiddleware = (req, res, next) => {
+//   const currentUser = req.user;
+//   const urlId = req.params.userId;
+//   console.log(currentUser, urlId);
+//   if (currentUser.id !== req.params.userId){
+//     const error = new Error('Unauthorized request');
+//     error.status = 401;
+//     next(error);
+//   } else {
+//     next();
+//   }
+// };
+
+module.exports = {
+  isAdminMiddleware
+};
