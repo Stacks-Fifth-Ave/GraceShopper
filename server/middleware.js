a7b4973f53ac26a621a0c97455b661ffacdbe484
Camryn Perry <camrynpaigeperry@gmail.com>
2019-11-07 20:47:03 -0500

    refactor: route security middleware for cart

--- server/middleware.js
+++ server/middleware.js
@@ -11,14 +11,12 @@ const isAdminMiddleware = (req, res, next) => {
   }
 };
 const isCurrentUserMiddleware = (req, res, next) => {
-  const currentUser = req.user;
-  const urlId = req.params.userId;
-  if (currentUser.id !== +req.params.userId) {
+  if (req.user.id === +req.params.userId || req.user.isAdmin) {
+    next();
+  } else {
     const error = new Error('Unauthorized request');
     error.status = 401;
     next(error);
-  } else {
-    next();
   }
 };
 
