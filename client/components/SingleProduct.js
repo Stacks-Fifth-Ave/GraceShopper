bfaea086dde3f0fb8bfcb5ad81027a5bca522e63
Malka <Malka.in.red@gmail.com>
2019-11-10 12:15:41 -0500

    added styling to all products and cart page

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -5,16 +5,30 @@ import {centsToDollarString, DEFAULT_CURRENCY} from '../formatters';
 
 const SingleProduct = props => {
   // convert cents to a string to represent the value in dollars
-
   return (
-    <div className="single-product">
-      <h1>{props.product.name}</h1>
-      <img src={props.product.image} />
-      <p>{props.product.description}</p>
-      <p>{centsToDollarString(props.product.price, DEFAULT_CURRENCY)}</p>
-      <button onClick={() => props.submit(props.product)} type="submit">
-        Add to Cart
-      </button>
+    <div className="single-product col">
+      <div className="card">
+        <div className="card-image center">
+          <img className="single-product-image" src={props.product.image} />
+          <span className="card-title">
+            {props.product.name}{' '}
+            {centsToDollarString(props.product.price, DEFAULT_CURRENCY)}
+          </span>
+        </div>
+        <div className="card-content center">
+          <p>{props.product.description}</p>
+          {/* <p>{centsToDollarString(props.product.price, DEFAULT_CURRENCY)}</p> */}
+        </div>
+        <div className="card-action center">
+          <button
+            className="waves-effect waves-light btn-large"
+            onClick={() => props.submit(props.product)}
+            type="submit"
+          >
+            Add to Cart
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,35 +6,62 @@ import {logout} from '../store';
 
 // temporarily taking implicit return out of this functional compoent
 const Navbar = props => (
-  <div>
-    <h1>Stacks Fifth Avenue</h1>
-    <nav>
-      <div>
-        <Link to="/cart">Cart</Link>
-        <Link to="/allProducts">All Products</Link>
-      </div>
-      {props.isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/orders">Order History</Link>
-          <Link to="/home">User Profile</Link>
-          <a href="#" onClick={props.handleClick}>
-            Logout
-          </a>
-          {props.userEmail}
-          <Link to="/allUsers">Manage Users</Link>
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          Guest
-        </div>
-      )}
-    </nav>
-    <hr />
-  </div>
+  <nav>
+    <div className="nav-wrapper">
+      <a href="#" className="brand-logo right">
+        Stacks Fifth Avenue
+      </a>
+      <ul id="nav-mobile" className="left hide-on-med-and-down">
+        <li>
+          <div>
+            <li>
+              <Link to="/cart">Cart</Link>
+            </li>
+            <li>
+              <Link to="/allProducts">All Products</Link>
+            </li>
+          </div>
+        </li>
+        {props.isLoggedIn ? (
+          <li>
+            <div>
+              {/* The navbar will show these links after you log in */}
+              <li>
+                <Link to="/orders">Order History</Link>
+              </li>
+              <li>
+                <Link to="/home">User Profile</Link>
+              </li>
+              <li>
+                <Link to="/allUsers">Manage Users</Link>
+              </li>
+              <li>
+                <a href="#" onClick={props.handleClick}>
+                  Logout
+                </a>
+              </li>
+              <li className="hide-on-large-and-down">
+                Logged in as {props.userEmail}
+              </li>
+            </div>
+          </li>
+        ) : (
+          <li>
+            <div>
+              {/* The navbar will show these links before you log in */}
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+              <li>
+                <Link to="/signup">Sign Up</Link>
+              </li>
+              <li>Viewing as guest</li>
+            </div>
+          </li>
+        )}
+      </ul>
+    </div>
+  </nav>
 );
 
 /**
