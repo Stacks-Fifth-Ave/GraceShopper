af9d4d0e9638052f3484810f313cb4ca1e6695a2
Stefanie Reyes <sreyes8@binghamton.edu>
2019-11-11 17:23:25 -0500

    fix: .gitignorefile

--- server/auth/google.js
+++ server/auth/google.js
@@ -2,7 +2,7 @@ const passport = require('passport');
 const router = require('express').Router();
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 const {User} = require('../db/models');
-const {clientID, clientSecret, callbackURL} = require('./secrets');
+const {clientID, clientSecret, callbackURL} = require('../../secrets');
 module.exports = router;
 
 /**
