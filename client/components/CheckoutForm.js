64e03cad0daede2cee6663ec24e8f802e49c2ae2
Camryn Perry <camrynpaigeperry@gmail.com>
2019-11-12 15:46:34 -0500

    feat: redirect to home page after submitting order

--- client/components/CheckoutForm.js
+++ client/components/CheckoutForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Checkout from './Checkout';
 import {clearProducts} from '../store/cart';
 import {connect} from 'react-redux';
+import {Redirect} from 'react-router-dom';
 
 class CheckoutForm extends React.Component {
   constructor() {
@@ -72,7 +73,8 @@ class CheckoutForm extends React.Component {
         'MP',
         'PR',
         'VI'
-      ]
+      ],
+      redirect: false
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -82,6 +84,7 @@ class CheckoutForm extends React.Component {
     e.preventDefault();
     if (this.props.paid) {
       this.props.clearProducts();
+      this.setState({redirect: true});
     } else {
       alert('please enter a valid card');
     }
@@ -94,139 +97,143 @@ class CheckoutForm extends React.Component {
   }
 
   render() {
-    return (
-      <div>
-        <form name="checkout-form">
-          <div>
-            <label>
-              <small>First Name</small>
-            </label>
-            <input
-              name="firstName"
-              type="text"
-              value={this.state.firstName}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <label>
-              <small>Last Name</small>
-            </label>
-            <input
-              name="lastName"
-              type="text"
-              value={this.state.lastName}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div />
-          <div>
-            <label htmlFor="streetAddress">
-              <small>Street Address</small>
-            </label>
-            <input
-              name="streetAddress"
-              type="text"
-              value={this.state.streetAddress}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="street">
-              <small>Street</small>
-            </label>
-            <input
-              name="street"
-              type="text"
-              value={this.state.street}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="aptNo">
-              <small>Apt. No.</small>
-            </label>
-            <input
-              name="aptNo"
-              type="text"
-              value={this.state.aptNo}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="city">
-              <small>City</small>
-            </label>
-            <input
-              name="city"
-              type="text"
-              value={this.state.city}
-              onChange={this.handleChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="state">
-              <small>State</small>
-            </label>
-            <select
-              name="state"
-              value={this.state.state}
-              onChange={this.handleChange}
-            >
-              {this.state.stateList.map((state, idx) => (
-                <option key={idx} value={state}>
-                  {state}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            <label htmlFor="zip">
-              <small>Zip</small>
-            </label>
-            <input
-              name="zip"
-              type="text"
-              value={this.state.zip}
-              onChange={this.handleChange}
-            />
-          </div>
-        </form>
+    if (this.state.redirect) {
+      return <Redirect to="/allProducts" />;
+    } else {
+      return (
         <div>
-          <Checkout
-            name="Checkout"
-            description="enter you account email"
-            amount={this.props.total}
-          />
+          <form name="checkout-form">
+            <div>
+              <label>
+                <small>First Name</small>
+              </label>
+              <input
+                name="firstName"
+                type="text"
+                value={this.state.firstName}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label>
+                <small>Last Name</small>
+              </label>
+              <input
+                name="lastName"
+                type="text"
+                value={this.state.lastName}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div />
+            <div>
+              <label htmlFor="streetAddress">
+                <small>Street Address</small>
+              </label>
+              <input
+                name="streetAddress"
+                type="text"
+                value={this.state.streetAddress}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="street">
+                <small>Street</small>
+              </label>
+              <input
+                name="street"
+                type="text"
+                value={this.state.street}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="aptNo">
+                <small>Apt. No.</small>
+              </label>
+              <input
+                name="aptNo"
+                type="text"
+                value={this.state.aptNo}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="city">
+                <small>City</small>
+              </label>
+              <input
+                name="city"
+                type="text"
+                value={this.state.city}
+                onChange={this.handleChange}
+              />
+            </div>
+            <div>
+              <label htmlFor="state">
+                <small>State</small>
+              </label>
+              <select
+                name="state"
+                value={this.state.state}
+                onChange={this.handleChange}
+              >
+                {this.state.stateList.map((state, idx) => (
+                  <option key={idx} value={state}>
+                    {state}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label htmlFor="zip">
+                <small>Zip</small>
+              </label>
+              <input
+                name="zip"
+                type="text"
+                value={this.state.zip}
+                onChange={this.handleChange}
+              />
+            </div>
+          </form>
+          <div>
+            <Checkout
+              name="Checkout"
+              description="enter you account email"
+              amount={this.props.total}
+            />
+          </div>
+          <div>
+            {this.state.firstName !== '' &&
+            this.state.lastName !== '' &&
+            this.state.streetAddress !== '' &&
+            this.state.street !== '' &&
+            this.state.city !== '' &&
+            this.state.state !== '' &&
+            this.state.zip.length === 5 ? (
+              <button
+                className="waves-effect waves-light btn-large"
+                type="submit"
+                onClick={this.handleSubmit}
+              >
+                Submit
+              </button>
+            ) : (
+              <button
+                className="waves-effect waves-light btn-large"
+                type="submit"
+                onClick={this.handleSubmit}
+                disabled
+              >
+                Submit
+              </button>
+            )}
+          </div>
         </div>
-        <div>
-          {this.state.firstName !== '' &&
-          this.state.lastName !== '' &&
-          this.state.streetAddress !== '' &&
-          this.state.street !== '' &&
-          this.state.city !== '' &&
-          this.state.state !== '' &&
-          this.state.zip.length === 5 ? (
-            <button
-              className="waves-effect waves-light btn-large"
-              type="submit"
-              onClick={this.handleSubmit}
-            >
-              Submit
-            </button>
-          ) : (
-            <button
-              className="waves-effect waves-light btn-large"
-              type="submit"
-              onClick={this.handleSubmit}
-              disabled
-            >
-              Submit
-            </button>
-          )}
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
