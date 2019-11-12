ccbe19627d83504a3e153e3e6d8a680cb9d647d1
Malka <Malka.in.red@gmail.com>
2019-11-12 12:08:25 -0500

    feat: add order history info

--- client/components/OrderHistory.js
+++ client/components/OrderHistory.js
@@ -2,13 +2,15 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 import {getOrders} from '../store/orderHistory';
+import SingleProduct from './SingleProduct';
+import {centsToDollarString, DEFAULT_CURRENCY} from '../formatters.js';
 
 class DisconnectedOrderHistory extends React.Component {
   componentDidMount() {
     this.props.getOrders();
   }
   render() {
-    // console.log(this.props.orders);
+    console.log('orders', this.props.orders);
     if (!this.props.orders.length) {
       return (
         <div>
@@ -16,10 +18,30 @@ class DisconnectedOrderHistory extends React.Component {
         </div>
       );
     }
+    let orderNum = 1;
     return (
       <div>
         {this.props.orders.map(order => (
-          <div key={order.id}>Order number:{order.id}</div>
+          <div key={order.id} className="card">
+            <p>Order #{orderNum++}</p>
+            {order.products.map(product => (
+              <div key={product.id}>
+                <div className="row">
+                  name: {product.name}
+                  <br />
+                  price: {centsToDollarString(product.price, DEFAULT_CURRENCY)}
+                  <br />
+                  quantity: {product.CartProduct.quantity} <br />
+                  total price:{' '}
+                  {centsToDollarString(
+                    product.price * product.CartProduct.quantity,
+                    DEFAULT_CURRENCY
+                  )}
+                  <br />
+                </div>
+              </div>
+            ))}
+          </div>
         ))}
       </div>
     );
